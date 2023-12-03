5a7260234c14406a71e238d6b0efe9535d57180d
JaronOris <jaronoris@protonmail.com>
2023-12-03 17:15:01 -0500

    Add useAppState & name change button

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,9 +1,36 @@
 import React from "react";
+import useAppState from "@/useHooks/useAppState";
+import { Grid, Button, Header } from "semantic-ui-react";
 
 export default function Favorites() {
+  const appState = useAppState();
+
+  console.log(appState);
+
+  function changeName() {
+    const titles = ["Little", "Big", "Mungo"];
+    const randomTitleIndex = Math.floor(Math.random() * titles.length);
+
+    appState.updateAppState({
+      user: `The ${titles[randomTitleIndex]} Guy`,
+    });
+  }
+
   return (
     <>
-      <h1>Favorites</h1>
+      <Grid columns="1">
+        <Grid.Column>
+          <Header as="h1">{appState.user}'s Favorites</Header>
+        </Grid.Column>
+        <Grid.Column>
+          <Button
+            content="Random Name"
+            color="blue"
+            icon="sync"
+            onClick={changeName}
+          />
+        </Grid.Column>
+      </Grid>
     </>
   );
 }
